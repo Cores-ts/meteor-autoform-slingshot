ab895067375e5eda378c839144d7b198310d12e9
Jose Alonso García <josealonsogarcia@gmail.com>
2018-01-23 16:55:50 +0100

    Updated with gear data and added show errors

--- fileRecord.js
+++ fileRecord.js
@@ -1,128 +1,148 @@
+afSlingshot = {};
+
+afSlingshot.fileSchema = new SimpleSchema({
+    'src': {type: String},
+    'key': {type: String},
+    'filename': {type: String},
+    'type': {type: String},
+    'size': {type: Number},
+    'directive': {type: String},
+});
+
 // Kind of using 2 different modes for file param.  Ugly.
-afSlingshot.FileRecord = function(file, config, newUpload) {
-  var self = this;
-  if ( !file ) {
-    throw new Meteor.Error('No file given');
-  }
-  
-  self._config = _.extend( {},config );
-  self._data = {};
-  self._dep = new Deps.Dependency();
-  self._downloadUrl = new ReactiveVar();
+afSlingshot.FileRecord = function (file, config, newUpload) {
+    var self = this;
+    if (!file) {
+        throw new Meteor.Error('No file given');
+    }
 
+    self._config = _.extend({}, config);
+    self._data = {};
+    self._dep = new Deps.Dependency();
+    self._downloadUrl = new ReactiveVar();
 
-  if( newUpload ){
-    _.each(self._config.directives, function(directive) {
-      self._upload( file, directive );
-    });
-  }else{
-    _.each(file, function(obj) {
-      if( !self._defaultSet ){
-        self._setDefault( obj );
-      }
-      self._data[ obj.directive ] = obj;
-    });
-  }
+    self._error = new ReactiveVar();
+
+    if (newUpload) {
+        _.each(self._config.directives, function (directive) {
+            self._upload(file, directive, function (err) {
+                self._error.set( err.error);
+            });
+        });
+    } else {
+        _.each(file, function (obj) {
+            if (!self._defaultSet) {
+                self._setDefault(obj);
+            }
+            self._data[obj.directive] = obj;
+        });
+    }
 };
 
-afSlingshot.FileRecord.prototype._setDefault = function(f){
-  var self = this;
-  _.defaults(self, f);
-  self._defaultSet = true;
-  
-  if (self._config.downloadUrl) {
-    self._config.downloadUrl(self,function(resp) {
-      self._downloadUrl.set(resp.src);
-    });
-  }
+afSlingshot.FileRecord.prototype._setDefault = function (f) {
+    var self = this;
+    _.defaults(self, f);
+    self._defaultSet = true;
 
-  self._dep.changed();
-};
+    if (self._config.downloadUrl) {
+        self._config.downloadUrl(self, function (resp) {
+            self._downloadUrl.set(resp.src);
+        });
+    }
 
-afSlingshot.FileRecord.prototype._registerDirectiveUpload = function( u, directive ){
-  var f = {
-    src: u.url(),
-    filename: u.file.name,
-    type: u.file.type,
-    size: u.file.size,
-    uploader: u,
-    directive: directive.name
-  };
-
-  this._data[ directive.name ] = f;
-  if( !this._defaultSet ){
-    this._setDefault( f );
-  }
-};
-
-afSlingshot.FileRecord.prototype._upload = function(file, directive, callback){
-  var self = this;
-  var uploader = new Slingshot.Upload(directive.name);
-
-  // Save directive name to uploader object, since it's not avaliable
-  uploader.directive = directive.name;
-
-  var uploaderCallback = function(err, downloadUrl) {
-    if (err) { console.error(err); }
-
-    self._setProp(directive.name, 'src', downloadUrl);
-    self._setProp(
-      directive.name,
-      'key',
-      _.findWhere(uploader.instructions.postData, {name: 'key'}).value
-    );
     self._dep.changed();
-  }
+};
 
-  if (directive.onBeforeUpload) {
-    directive.onBeforeUpload(file, function(err, file) {
-      if( err ){ console.error( err ); }
-      uploader.send(file, uploaderCallback);
-      self._registerDirectiveUpload( uploader, directive );
-    });
-  }else {
-    uploader.send(file, uploaderCallback);
-    self._registerDirectiveUpload( uploader, directive );
-  }
+afSlingshot.FileRecord.prototype._registerDirectiveUpload = function (u, directive) {
+    var f = {
+        src: u.url(),
+        filename: u.file.name,
+        type: u.file.type,
+        size: u.file.size,
+        uploader: u,
+        directive: directive.name
+    };
+
+    this._data[directive.name] = f;
+    if (!this._defaultSet) {
+        this._setDefault(f);
+    }
+};
+
+afSlingshot.FileRecord.prototype._upload = function (file, directive, callback) {
+    var self = this;
+    var uploader = new Slingshot.Upload(directive.name);
+
+    // Save directive name to uploader object, since it's not avaliable
+    uploader.directive = directive.name;
+
+    var uploaderCallback = function (err, downloadUrl) {
+        if (err) {
+            console.error('uploaderCallback ' + err);
+            // throw new Meteor.Error('uploaderCallback ' + err.error);
+            callback(err);
+        }
+
+        self._setProp(directive.name, 'src', downloadUrl);
+        self._setProp(
+            directive.name,
+            'key',
+            _.findWhere(uploader.instructions.postData, {name: 'key'}).value
+        );
+        self._dep.changed();
+    }
+
+    if (directive.onBeforeUpload) {
+        directive.onBeforeUpload(file, function (err, file) {
+            if (err) {
+                console.error('onbeforeupload' + err);
+            }
+            uploader.send(file, uploaderCallback);
+            self._registerDirectiveUpload(uploader, directive);
+        });
+    } else {
+        uploader.send(file, uploaderCallback);
+        self._registerDirectiveUpload(uploader, directive);
+    }
 }
 
-afSlingshot.FileRecord.prototype._setProp = function(directive, key, val) {
-  this._data[ directive ][ key ] = val;
+afSlingshot.FileRecord.prototype._setProp = function (directive, key, val) {
+    this._data[directive][key] = val;
 };
 
-afSlingshot.FileRecord.prototype.getData = function() {
-  this._dep.depend();
+afSlingshot.FileRecord.prototype.getData = function () {
+    this._dep.depend();
 
-  var self = this;
-  var data = _.map(self._data, function(v, k) {
-    return self._data[ k ];
-  });
-  return data;
+    var self = this;
+    var data = _.map(self._data, function (v, k) {
+        return self._data[k];
+    });
+    return data;
 };
 
-afSlingshot.FileRecord.prototype.progress = function() {
-  this._dep.depend();
+afSlingshot.FileRecord.prototype.progress = function () {
+    this._dep.depend();
 
-  var min = _.min(this.getData(), function(file) {
-    if (file.uploader) {
-      if (file.uploader.progress) {
-        return file.uploader.progress();
-      }else {
-        return 0;
-      }
-    }else {
-      return 1;
+    var min = _.min(this.getData(), function (file) {
+        if (file.uploader) {
+            if (file.uploader.progress) {
+                return Math.round(file.uploader.progress());
+            } else {
+                return 0;
+            }
+        } else {
+            return 1;
+        }
+    });
+    if (min.uploader) {
+        if (min.uploader.progress) {
+            return Math.round(min.uploader.progress() * 100);
+        } else {
+            return 0;
+        }
+    } else {
+        return 100;
     }
-  });
-  if (min.uploader) {
-    if (min.uploader.progress) {
-      return min.uploader.progress() * 100;
-    }else {
-      return 0;
-    }
-  }else {
-    return 100;
-  }
 };
 
 // There are 3 distinct modes for download.
@@ -130,32 +150,37 @@ afSlingshot.FileRecord.prototype.progress = function() {
 // downloadUrl present ( likely meaning user has to auth from server )
 // otherwise just use raw src value
 // fetch a download url and save it in reactive variable
-afSlingshot.FileRecord.prototype.downloadUrl = function() {
-  if (this.uploader) {
-    return this.uploader.url();
-  }if (this._config.downloadUrl) {
-    return this._downloadUrl.get();
-  }else {
-    return this.src;
-  }
+afSlingshot.FileRecord.prototype.downloadUrl = function () {
+    if (this.uploader) {
+        return this.uploader.url();
+    }
+    if (this._config.downloadUrl) {
+        return this._downloadUrl.get();
+    } else {
+        return this.src;
+    }
 };
 
-afSlingshot.FileRecord.prototype.downloadAvaliable = function() {
-  if (this._config.downloadUrl && !this.uploader) {
-    return this._downloadUrl.get() !== undefined;
-  }else {
-    return true;
-  }
+afSlingshot.FileRecord.prototype.downloadAvaliable = function () {
+    if (this._config.downloadUrl && !this.uploader) {
+        return this._downloadUrl.get() !== undefined;
+    } else {
+        return true;
+    }
 };
 
-afSlingshot.FileRecord.prototype.download = function() {
-  if (this.downloadAvaliable()) {
-    return window.open(this.downloadUrl());
-  }else {
-    console.error('Attempted to download file before download url was set.');
-  }
+afSlingshot.FileRecord.prototype.download = function () {
+    if (this.downloadAvaliable()) {
+        return window.open(this.downloadUrl());
+    } else {
+        console.error('Attempted to download file before download url was set.');
+    }
 };
 
-afSlingshot.FileRecord.prototype.isImage = function() {
-  return Boolean(this.type && this.type.split("/")[0] === "image");
+afSlingshot.FileRecord.prototype.isImage = function () {
+    return Boolean(this.type && this.type.split("/")[0] === "image");
 };
+
+afSlingshot.FileRecord.prototype.error = function () {
+    return this._error.get();
+}
--- template.js
+++ template.js
@@ -1,163 +1,198 @@
-var hackySlingshotStorage = {};  // TODO WTF? autoform.  Seriously no way to get an instance variable in valueOut?
-SimpleSchema.messages({uploadInProgress: 'Upload in progress'});
+var hackySlingshotStorage = {}; // TODO WTF? autoform.  Seriously no way to get an instance variable in valueOut?
+SimpleSchema.messages({ uploadInProgress: 'Upload in progress' });
+SimpleSchema.messages({ wrongFilename: 'Filename not valid' });
+
 AutoForm.addInputType('slingshot', {
-  template: 'afSlingshot',
-  valueOut: function() {
-    var afSlingshot = hackySlingshotStorage[ this.attr('data-schema-key') ];
-    var validationContext = AutoForm.getValidationContext();
+    template: 'afSlingshot',
+    valueOut: function() {
+        var afSlingshot = hackySlingshotStorage[this.attr('data-schema-key')];
+        //var validationContext = AutoForm.getValidationContext('blockUpdate');
 
-    var val;
-    if (this.attr('data-multiple')) {
-      val = afSlingshot.data();
-    }else {
-      val =  afSlingshot.data()[0];
-    }
-
-    // validate
-    if (afSlingshot.progress() < 100) {
-      validationContext.addInvalidKeys([{
-        name: this.attr('data-schema-key'),
-        type: 'uploadInProgress',
-        value: val
-      }]);
-    }else {
-      return val;
-    }
-  },
-  valueConverters: {
-    'string': function(val) {
-      return val.src;
-    }
-  },
-  valueIn: function(val) {
-    if (typeof(val) === 'undefined' || val === '' || val === null) { return; }
-
-    var data = {};
-    if (typeof(val) === 'string') {
-      var f = {
-        src: val,
-        filename: val,
-        key: val
-      };
-      data[ f.filename ] = [f];
-    }else if (!_.isArray(val)) {
-      data[ val.filename] = [_.extend({},val)];
-    }else {
-      _.each(val, function(file) {
-        if (data[ file.filename ] === undefined) {
-          data[ file.filename ] = [];
+        var val;
+        if (this.attr('data-multiple')) {
+            val = afSlingshot.data();
+        } else {
+            val = afSlingshot.data()[0];
         }
-        data[ file.filename ].push(_.extend({},file));
-      });
+
+        //console.log(this)
+        //console.log("valslingdhot", val)
+
+        // validate
+        if (afSlingshot.progress() < 100) {
+            /*validationContext.addInvalidKeys([{
+                name: this.attr('data-schema-key'),
+                type: 'uploadInProgress',
+                value: val
+            }]);*/
+            AutoForm.addStickyValidationError('blockUpdate', this.attr('data-schema-key'), "uploadInProgress", val)
+        } else if ((typeof val == "undefined" || val == "") && this[0].required) {
+            /*validationContext.addInvalidKeys([{
+                name: this.attr('data-schema-key'),
+                type: 'required'
+            }]);*/
+            AutoForm.addStickyValidationError('blockUpdate', this.attr('data-schema-key'), "required", val)
+        } else if ((typeof val != "undefined" || val != "") && (val && val !=  "undefined" && val.src && val.src.startsWith("blob:"))) {
+            /*validationContext.addInvalidKeys([{
+                name: this.attr('data-schema-key'),
+                type: 'wrongFilename',
+                value: val
+            }]);*/
+            AutoForm.addStickyValidationError('blockUpdate', this.attr('data-schema-key'), "wrongFilename", val)
+        } else {
+            AutoForm.removeStickyValidationError('blockUpdate', this.attr('data-schema-key'))
+            return val;
+        }
+    },
+    valueConverters: {
+        'string': function(val) {
+            return val.src;
+        }
+    },
+    valueIn: function(val) {
+        if (typeof(val) === 'undefined' || val === '' || val === null) { return; }
+        //console.log(val)
+        var data = {};
+        if (typeof(val) === 'string') {
+            var f = {
+                src: val,
+                filename: val,
+                key: val
+            };
+            data[f.filename] = [f];
+        } else if (!_.isArray(val)) {
+            data[val.filename] = [_.extend({}, val)];
+        } else {
+            _.each(val, function(file) {
+                if (data[file.filename] === undefined) {
+                    data[file.filename] = [];
+                }
+                data[file.filename].push(_.extend({}, file));
+            });
+        }
+
+        return data;
     }
-    return data;
-  }
 });
 
 Template.afSlingshot.onCreated(function() {
-  this.afSlingshot = new afSlingshot.Model(
-    this.data.atts.slingshot,
-    this.data.value,
-    this.data.atts.multi
-  );
+    this.afSlingshot = new afSlingshot.Model(
+        this.data.atts.slingshot,
+        this.data.value,
+        this.data.atts.multi
+    );
 
-  // Seriously... fuck you
-  hackySlingshotStorage[ this.data.atts['data-schema-key'] ] = this.afSlingshot;
+    // Seriously... fuck you
+    hackySlingshotStorage[this.data.atts['data-schema-key']] = this.afSlingshot;
 
-  // Setup UI defaults
-  this._config = this.data.atts.ui ? this.data.atts.ui : {};
-  if (this.data.atts.multi) {
-    _.defaults(this._config,{
-      hideList: false,
-      hideIcons: false,
-      noPreview: false,
-      multipleUpload: true,
-      hideDownload: true
-    });
-    // need to add this so we can tell that we're dealing with multiple files later
-    this.data.atts['data-multiple'] = true;
-  }else {
-    _.defaults(this._config,{
-      hideList: true,
-      multipleUpload: false
-    });
-  }
+    // Setup UI defaults
+    this._config = this.data.atts.ui ? this.data.atts.ui : {};
+    if (this.data.atts.multi) {
+        _.defaults(this._config, {
+            hideList: false,
+            hideIcons: false,
+            noPreview: false,
+            multipleUpload: true,
+            hideDownload: true
+        });
+        // need to add this so we can tell that we're dealing with multiple files later
+        this.data.atts['data-multiple'] = true;
+    } else {
+        _.defaults(this._config, {
+            hideList: true,
+            multipleUpload: false
+        });
+    }
 });
 
 Template.afSlingshot.onDestroyed(function() {
-  delete hackySlingshotStorage[ this.data.atts.id ];
+    delete hackySlingshotStorage[this.data.atts.id];
 });
 
 Template.afSlingshot.helpers({
-  atts: function() {
-    return _.omit(this.atts, 'ui','slingshot','multi');
-  },
-  config: function() {
-    var t = Template.instance();
-    return t._config;
-  },
-  allowedFileTypes: function() {
-    var t = Template.instance();
-    var allowedFileTypes = t.afSlingshot.allowedFileTypes();
-    return allowedFileTypes;
-  },
-  files: function() {
-    var t = Template.instance();
-    return t.afSlingshot.files();
-  },
-  summary: function() {
-    var t = Template.instance();
-    var files = t.afSlingshot.files();
-    if (files.length === 0) {
-      return 'Click to upload';
-    }else if (files.length === 1) {
-      return files[0].filename;
-    }else {
-      return files.length.toString() + ' files uploaded';
+    atts: function() {
+        return _.omit(this.atts, 'ui', 'slingshot', 'multi')
+    },
+    config: function() {
+        var t = Template.instance();
+        return t._config;
+    },
+    allowedFileTypes: function() {
+        var t = Template.instance();
+        var allowedFileTypes = t.afSlingshot.allowedFileTypes();
+        return allowedFileTypes;
+    },
+    files: function() {
+        var t = Template.instance();
+        return t.afSlingshot.files();
+    },
+    summary: function() {
+        var t = Template.instance();
+        var files = t.afSlingshot.files();
+        if (files.length === 0) {
+            return 'Click to upload';
+        } else if (files.length === 1) {
+            return files[0].filename.substring(files[0].filename.lastIndexOf('/') + 1);
+        } else {
+            return files.length.toString() + ' files uploaded';
+        }
+    },
+    showList: function() {
+        var t = Template.instance();
+        return t.afSlingshot.files().length > 0 && !t._config.hideList;
+    },
+    progressAll: function() {
+        var t = Template.instance();
+        return t.afSlingshot.progress();
+    },
+    showTotalProgress: function() {
+        var t = Template.instance();
+        var progress = t.afSlingshot.progress();
+        if (progress !== 0 && progress < 100) {
+            return true;
+        }
+    },
+    round: function(val) {
+        if (typeof(val) === 'number') {
+            return Math.round(val);
+        }
+    },
+    mimeClass: function() { // from file scope
+        var t = Template.instance();
+        if (this.type) {
+            return this.type.replace('/', '-');
+        }
+    },
+    docName: function(url) { // from file scope
+        if (url) {
+            return url.substring(url.lastIndexOf('/') + 1);
+        }
+    },
+    err: function () {
+        var t = Template.instance();
+        console.log(t.afSlingshot.error(this.filename));
+        return t.afSlingshot.error(this.filename);
     }
-  },
-  showList: function() {
-    var t = Template.instance();
-    return t.afSlingshot.files().length > 0 && !t._config.hideList;
-  },
-  progressAll: function() {
-    var t = Template.instance();
-    return t.afSlingshot.progress();
-  },
-  showTotalProgress: function() {
-    var t = Template.instance();
-    var progress = t.afSlingshot.progress();
-    if (progress !== 0 && progress < 100) {
-      return true;
-    }
-  },
-  round: function(val) {
-    if (typeof(val) === 'number') {
-      return Math.round(val);
-    }
-  },
-  mimeClass: function() { // from file scope
-    var t = Template.instance();
-    if (this.type) {
-      return this.type.replace('/','-');
-    }
-  }
 });
 
 Template.afSlingshot.events({
-  'change .file-upload': function(e, t) {
-    t.afSlingshot.add(e.target.files);
-  },
-  'click div[name="remove"]': function(e, t) {
-    t.afSlingshot.remove(this.filename);
-  },
-  'click *[name="afSlingshot-uploader"]': function(e, t) {
-    t.$('input[type="file"]').click();
-  },
-  'click .download-btn': function(e, t) {
-    e.stopPropagation();
-    t.afSlingshot.downloadAll();
-  }
+    'change .file-upload': function(e, t) {
+        t.afSlingshot.add(e.target.files);
+    },
+    'click div[name="remove"]': function(e, t) {
+        t.afSlingshot.remove(this.filename);
+    },
+    'click *[name="afSlingshot-uploader"]': function(e, t) {
+        t.$('input[type="file"]').click();
+    },
+    'click .download-btn': function(e, t) {
+        e.stopPropagation();
+        t.afSlingshot.downloadAll();
+    },
+    'click .download-btn2': function(e, t) {
+        e.stopPropagation();
+        t.afSlingshot.error(this.filename);
+    }
 });
 
 /*
